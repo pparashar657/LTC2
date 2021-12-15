4032b10837290fa881022692629eb5d75f0ada53
Pawan Kumar <pkumrbh@amazon.com>
2021-12-15 09:11:16 +0530

    Introduction

---
+++ introduction/InputOutput.java
@@ -0,0 +1,79 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+	public static void main(String[] args) {
+//		System.out.println("Pawan is okay");
+//		
+//		System.out.println((637 + 16) * 6);
+//		int a = 12;
+//		System.out.println("Pawan roll No = " + a + " and my age is " + 22 + true);
+//		System.out.println(12 + 13);
+//		System.out.println(12 + " " + 13);
+//		
+//		System.out.println(12 + 34.87);
+//		This is a single line comment
+		
+		
+		/*
+		 
+		 This is a multi line comment
+		  
+		 fkldes
+		 dmsfvklsdf
+		 kvl;rsfd
+		 jvsrf
+		 kvsodmsopdr
+		 vopds
+		 */
+		
+//		System.out.println("Enter any number : ");
+//		
+		Scanner sc = new Scanner(System.in);
+//		
+//		int a = sc.nextInt();
+//		
+//		System.out.println("The number is : " + a);
+//		
+//		float b = sc.nextFloat();
+//		
+//		System.out.println("Float num = " + b);
+//		
+//		long c = sc.nextLong();
+//		
+//		System.out.println("Long num = " + c);
+//		
+//		System.out.println("Enter anything");
+//		
+//		String str1 = sc.next();
+//		
+//		System.out.println("String input = " + str1);
+////		
+//		String str2 = sc.next();
+//		
+//		System.out.println("String input = " + str2);
+//		
+//		String strLine = sc.nextLine();
+//		
+//		System.out.println("String line input = " + strLine);
+		
+		
+//		String str = "";
+//		
+//		System.out.println(str);
+//		
+//		char ch = 'a';
+//		
+//		System.out.println(ch);
+		System.out.println("Enter a char");
+		
+		char ch = sc.next().charAt(0);
+		
+		System.out.println(ch);
+		
+		
+	}
+
+}
