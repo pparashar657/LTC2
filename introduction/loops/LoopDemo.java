ee88bc158942e676089220a2e9a780f12077b552
Pawan Kumar <pkumrbh@amazon.com>
2021-12-23 08:59:35 +0530

    loops introduction

---
+++ introduction/loops/LoopDemo.java
@@ -0,0 +1,107 @@
+package introduction.loops;
+
+import java.util.Scanner;
+
+public class LoopDemo {
+
+	public static void main(String[] args) {
+		
+//		while(true) {
+//			System.out.println("ok");
+//		}
+		
+		
+//		int num = 1;
+//		
+//		System.out.println("The numbers");
+//		
+//		while( num <= 10) {
+//			System.out.println(num);
+//			num += 1;
+//		}
+		
+		// Using for loop
+		
+//		for(int num = 1; num <= maxNum; num++) {
+//			System.out.println(num);
+//		}
+		
+		// Using do while
+		
+//		int num = 1;
+//		
+//		do {
+//			System.out.println(num);
+//			num++;
+//		} while(num <= 10);
+//		
+		
+		
+		
+//		
+//		System.out.println("Are these.");
+//		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter any number");
+		
+		int maxNum = sc.nextInt();
+//		
+//		int num = 1;
+//		
+//		do {
+//			System.out.println(num);
+//			num++;
+//		} while(num <= maxNum);
+		
+//		int num = 1;
+		
+		// Using while loop
+		
+//		while( num <= maxNum) {
+//			if(num %2 == 0) {
+//				System.out.println(num);
+//			}
+//			num += 1;
+//		}
+		
+		// Using for loop
+		
+//		for(int num = 1; num <= maxNum; num++) {
+//			if(num %2 == 0) {
+//				System.out.println(num);
+//			}
+//		}
+		
+//		int num = 1;
+//		
+//		do {
+//			if(num %2 == 0) {
+//				System.out.println(num);
+//			}
+//			num++;
+//		} while(num <= maxNum);
+		
+		
+		
+//		num = 2;
+		
+//		while(num <= maxNum) {
+//			System.out.println(num);
+//			
+//			num += 2;
+//		}
+		
+//		for(int num = 2; num <= maxNum; num += 2) {
+//			System.out.println(num);
+//		}
+		
+		
+		
+		
+
+	}
+
+}
---
+++ introduction/loops/TableOfN.java
@@ -0,0 +1,36 @@
+package introduction.loops;
+
+import java.util.Scanner;
+
+public class TableOfN {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter any num : ");
+		
+		int num = sc.nextInt();
+		
+//		for (int i = 1; i<= 10; i++) {
+//			System.out.println(num +" * " + i +" = " + (num * i));
+//		}
+		
+//		int i = 1;
+//		
+//		while(i <= 10) {
+//			System.out.println(num +" * " + i +" = " + (num * i));
+//			
+//			i++;
+//		}
+		int i = 0;
+		
+		do {
+			i++;
+			System.out.println(num +" * " + i +" = " + (num * i));
+			
+		} while(i <= 10);
+		
+
+	}
+
+}
